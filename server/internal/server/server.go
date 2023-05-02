14adbd55049fe12205893dfff7989e1962db6c42
RomanIkonnikov93 <96195014+RomanIkonnikov93@users.noreply.github.com>
2023-05-02 22:06:18 +0500

    fix server shutdown gracefully

--- server/internal/server/server.go
+++ server/internal/server/server.go
@@ -5,6 +5,7 @@ import (
 	"net"
 	"os"
 	"os/signal"
+	"sync"
 	"syscall"
 
 	"github.com/RomanIkonnikov93/keeper/server/internal/config"
@@ -18,31 +19,31 @@ import (
 // StartServer starts the server.
 func StartServer(rep repository.Reps, cfg config.Config, logger *logging.Logger) error {
 
-	idleConnsClosed := make(chan struct{})
+	listen, err := net.Listen("tcp", cfg.GRPCPort)
+	if err != nil {
+		logger.Fatal("net.Listen: ", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterKeeperServer(s, gapi.InitServices(rep, cfg, logger))
+
 	sigint := make(chan os.Signal, 1)
 	signal.Notify(sigint, syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT)
 
+	wg := sync.WaitGroup{}
+	wg.Add(1)
 	go func() {
-		listen, err := net.Listen("tcp", cfg.GRPCPort)
-		if err != nil {
-			logger.Fatal("net.Listen: ", err)
-		}
-		s := grpc.NewServer()
-		pb.RegisterKeeperServer(s, gapi.InitServices(rep, cfg, logger))
-
-		go func() {
-			<-sigint
-			s.GracefulStop()
-		}()
-
-		logger.Info("gRPC server running")
-		if err = s.Serve(listen); err != nil {
-			logger.Fatal(err)
-		}
+		<-sigint
+		logger.Println("server shutdown gracefully")
+		s.GracefulStop()
+		wg.Done()
 	}()
 
-	<-idleConnsClosed
-	logger.Println("Servers Shutdown gracefully")
+	logger.Info("gRPC server running")
+	if err = s.Serve(listen); err != nil {
+		logger.Fatal(err)
+	}
+	wg.Wait()
 
 	return nil
 }
